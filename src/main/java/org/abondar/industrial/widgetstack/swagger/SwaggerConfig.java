0cf0e52c25bef7268abb2c48a771d056e884cc7c
abondar24 <abondar1992@gmail.com>
2021-02-27 23:09:31 +0100

    version upgrade

--- src/main/java/org/abondar/industrial/widgetstack/swagger/SwaggerConfig.java
+++ src/main/java/org/abondar/industrial/widgetstack/swagger/SwaggerConfig.java
@@ -17,7 +17,7 @@ public class SwaggerConfig {
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("org.abondar.industrial.widgetstack.controller"))
+                .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
     }
